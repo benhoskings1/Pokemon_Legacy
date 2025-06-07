5cc40138a0d16d32292ba755d0ac979f8388ab49
“benhoskings1” <bensebandraf@gmail.com>
2025-06-07 18:36:41 +0100

    updates to some battle display functions

--- displays/menu/menu_display_team.py
+++ displays/menu/menu_display_team.py
@@ -3,9 +3,10 @@ from enum import Enum
 
 import pygame as pg
 
+from pokemon import Pokemon, getImages
 from general.utils import Colours
-from screen_V2 import FontOption
-from sprite_screen import SpriteScreen, DisplayContainer
+from screen_V2 import FontOption, BlitLocation
+from sprite_screen import SpriteScreen, DisplayContainer, GameObjects
 from pokemon import PokemonSpriteSmall
 
 
@@ -14,10 +15,15 @@ CONTAINER_POSITIONS = [(1, 3), (129, 12), (1, 52), (129, 60), (1, 100), (129, 10
 
 class MenuTeamDisplayStates(Enum):
     home = 0
+    summary = 1
     pokemon_info = 1
     trainer_memo = 2
     pokemon_skills = 3
     battle_moves = 4
+
+
+class PokemonSummaryStates(Enum):
+    info = "info"
 
 
 class PokemonContainer(DisplayContainer):
@@ -75,6 +81,30 @@ class PokemonContainer(DisplayContainer):
         return display_surf
 
 
+class PokemonInfoContainer(DisplayContainer):
+    def __init__(self, scale=1):
+        DisplayContainer.__init__(
+            self,"assets/containers/menu/team/info_container.png", PokemonSummaryStates.info, (104, 30),
+            scale=scale
+        )
+
+        labels = ["Pokedex No.", "Name", "Type", "OT", "ID No.", "Exp. Points", "", "To Next Lv."]
+        for idx, label in enumerate(labels):
+            self.add_text_2(
+                label, pg.Rect(pg.Vector2(8, 13 + 16*idx) * self.scale, pg.Vector2(80, 12)*self.scale),
+                colour=Colours.white, shadow_colour=Colours.darkGrey, base=True
+            )
+
+    def load_pokemon_details(self, pokemon: Pokemon):
+        values = [f"{pokemon.ID:03}", pokemon.name, "", "Ben", "46649", "",
+                  f"{pokemon.exp}", "", f"{pokemon.level_up_exp - pokemon.exp}"]
+        for idx, value in enumerate(values):
+            self.addText(
+                value, pg.Vector2(pg.Vector2(112, 13 + 16 * idx))* self.scale,
+                colour=Colours.black.value, shadowColour=Colours.lightGrey.value, base=True, location=BlitLocation.midTop
+            )
+
+
 class MenuTeamDisplayHome(SpriteScreen):
     def __init__(self, size, scale, team):
         SpriteScreen.__init__(self, size)
@@ -92,29 +122,84 @@ class MenuTeamDisplayHome(SpriteScreen):
 
         self.selected_idx = None
 
-    def update_containers(self, selected_idx):
+    def update_containers(self, selected_idx: int):
+        """
+        This function updates the screen by replacing the active display, but only updating the container to be
+        deselected, and the new one to be selected.
+
+        :param selected_idx:
+        :return:
+        """
         if self.selected_idx is None:
             self.selected_idx = 0
         else:
-            # handle killing of previous
-            ...
-        self.pk_containers[self.selected_idx].kill()
-        deselected_container = PokemonContainer(
-            self.team.pokemon[self.selected_idx], CONTAINER_POSITIONS[self.selected_idx], primary=selected_idx == 0,
-            scale=self.scale
-        )
-        self.pk_containers.insert(self.selected_idx, deselected_container)
+            self.get_object(self.pk_containers[self.selected_idx].pokemon).kill()
+            # self.pk_containers[self.selected_idx].kill()
+            deselected_container = PokemonContainer(
+                self.team.pokemon[self.selected_idx], CONTAINER_POSITIONS[self.selected_idx], primary=selected_idx == 0,
+                scale=self.scale
+            )
+            self.sprites.add(deselected_container)
 
-        self.pk_containers[selected_idx].kill()
+        self.get_object(self.pk_containers[selected_idx].pokemon).kill()
         selected_container = PokemonContainer(
             self.team.pokemon[selected_idx], CONTAINER_POSITIONS[selected_idx], primary=selected_idx == 0, selected=True,
             scale=self.scale
         )
-        self.pk_containers.insert(selected_idx, selected_container)
+        self.sprites.add(selected_container)
         self.selected_idx = selected_idx
 
         self.refresh()
-        self.sprites.add(selected_container)
+
+    def process_input(self, key, controller) -> None | Pokemon | str:
+        """
+        Process a user input.
+
+        :param key: user input.
+        :param controller: controller object
+        :return:
+        """
+        # ======= Processing Functions ========
+        if key == controller.a:
+            print(repr(self.pk_containers[self.selected_idx].pokemon))
+            return self.pk_containers[self.selected_idx].pokemon
+        elif key == controller.b:
+            return "back"
+
+        # ======= Move Selector ========
+        elif self.selected_idx is None:
+            self.update_containers(selected_idx=0)
+
+        elif key == controller.down:
+            self.update_containers(min([self.selected_idx + 1, len(self.sprites) - 1]))
+        elif key == controller.up:
+            self.update_containers(max([self.selected_idx - 1, 0]))
+
+        return None
+
+
+class MenuTeamDisplaySummary(SpriteScreen):
+    def __init__(self, size, pokemon, scale=1):
+        SpriteScreen.__init__(self, size)
+        self.scale = scale
+        self.pokemon = pokemon
+
+        self.load_image("assets/menu/team/summary_background.png", base=True, scale=scale)
+
+        front_image, _, _ = getImages(pokemon.ID, crop=False)
+        self.add_image(front_image, pg.Vector2(53, 106)*self.scale, base=True, location=BlitLocation.centre)
+
+        self.containers = {
+            PokemonSummaryStates.info: PokemonInfoContainer(scale=scale),
+        }
+
+        for container in self.containers.values():
+            container.load_pokemon_details(pokemon)
+
+        self.sprites.add(self.containers[PokemonSummaryStates.info])
+
+    def process_input(self, key, controller):
+        ...
 
 
 class MenuTeamDisplay:
@@ -123,6 +208,7 @@ class MenuTeamDisplay:
 
         self.displays = {
             MenuTeamDisplayStates.home: MenuTeamDisplayHome(size, scale, game.team),
+            MenuTeamDisplayStates.summary: MenuTeamDisplaySummary(size, game.team.pokemon[0], scale=scale),
         }
 
         self.active_display_state = MenuTeamDisplayStates.home
@@ -139,22 +225,14 @@ class MenuTeamDisplay:
         while True:
             for event in pg.event.get():
                 if event.type == pg.KEYDOWN:
-                    if (
-                        event.key in controller.keys and
-                        self.active_display_state == MenuTeamDisplayStates.home and
-                        self.active_display.selected_idx is None
-                    ):
-                        self.active_display.update_containers(selected_idx=0)
+                    if event.key in controller.keys:
+                        action = self.active_display.process_input(event.key, controller)
 
-                    elif event.key == controller.down:
-                        self.active_display.update_containers(
-                            selected_idx=min([self.active_display.selected_idx+1, len(self.active_display.sprites)-1])
-                        )
-                    elif event.key == controller.up:
-                        self.active_display.update_containers(
-                            selected_idx=min(
-                                [self.active_display.selected_idx - 1, 0])
-                        )
+                        if isinstance(action, Pokemon):
+                            self.active_display = self.displays[MenuTeamDisplayStates.summary]
+
+                        elif action == "back":
+                            return None
 
             self.update_display()
 
--- screen_V2.py
+++ screen_V2.py
@@ -170,9 +170,13 @@ class Screen:
         else:
             surf.blit(image, pos)
 
-    def add_text_2(self, text, text_box: pg.Rect, font_option: FontOption = FontOption.main,
+    def add_text_2(self, text:str, text_box: pg.Rect, font_option: FontOption = FontOption.main,
                    colour: Colours | pg.Color = None, shadow_colour: Colours | pg.Color = None,
-                   max_chars=None, base=False):
+                   max_chars=None, base=False) -> None | bool:
+
+        if len(text) == 0:
+            return False
+
         self.font = font_option.value
         text_surf, text_box = self.font.render_text_2(
             text, text_box, colour=colour, shadow_colour=shadow_colour, max_chars=max_chars
@@ -255,6 +259,8 @@ class Screen:
 
     def addText(self, text, pos, lines=1, location=BlitLocation.topLeft, base=False, colour=None,
                 shadowColour=None, fontOption: FontOption = FontOption.main, surface=None):
+        if len(text) == 0:
+            return False
 
         self.font = fontOption.value
 
